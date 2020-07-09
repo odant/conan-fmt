84269e021af3a10d9f01dc9bc65261afa71f6ef2
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-07-09 03:00:33 +0300

    First build

---
+++ conanfile.py
@@ -0,0 +1,91 @@
+# fmtlib Conan package
+# Dmitriy Vetutnev, Odant 2018 - 2020
+
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class GoogletestConan(ConanFile):
+    name = "fmt"
+    version = "7.0.0"
+    license = "https://raw.githubusercontent.com/fmtlib/fmt/master/LICENSE.rst"
+    description = "{fmt} is an open-source formatting library for C++. It can be used as a safe and fast alternative to (s)printf and iostreams."
+    url = "https://github.com/odant/conan-fmt"
+    settings = {
+        "os": ["Windows", "Linux"],
+        "compiler": ["Visual Studio", "gcc"],
+        "build_type": ["Debug", "Release"],
+        "arch": ["x86", "x86_64", "mips", "armv7"]
+    }
+    options = {
+        "with_unit_tests": [True, False],
+        "ninja": [True, False]
+    }
+    default_options = {
+        "with_unit_tests": False,
+        "ninja": True
+    }
+    generators = "cmake"
+    exports_sources = "src/*", "CMakeLists.txt"
+    no_copy_source = True
+    build_policy = "missing"
+
+    def build_requiments(self):
+        if self.options.ninja:
+            self.build_requires("ninja/1.9.0")
+
+    def build(self):
+        build_type = "RelWithDebInfo" if self.settings.build_type == "Release" else "Debug"
+        gen = "Ninja" if self.options.ninja == True else None
+        cmake = CMake(self, build_type=build_type, generator=gen, msbuild_verbosity='normal')
+        cmake.verbose = True
+        cmake.definitions["FMT_INSTALL"] = "ON"
+        cmake.definitions["FMT_DOC"] = "OFF"
+        if self.options.with_unit_tests:
+            cmake.definitions["FMT_TEST"] = "ON"
+        cmake.configure()
+        cmake.build()
+        if self.options.with_unit_tests:
+            if cmake.is_multi_configuration:
+                self.run("ctest --output-on-failure --build-config %s" % build_type)
+            else:
+                self.run("ctest --output-on-failure")
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib/cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "lib/pkgconfig"))
+
+    def package(self):
+        return
+        # CMake scripts
+        self.copy("FindGTest.cmake", dst=".", src=".", keep_path=False)
+        self.copy("FindGMock.cmake", dst=".", src=".", keep_path=False)
+        # Headers
+        self.copy("*.h", dst="include", src="src/googletest/include", keep_path=True)
+        self.copy("*.h", dst="include", src="src/googlemock/include", keep_path=True)
+        # Libraries
+        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        # PDB
+        self.copy("*gtest.pdb", dst="bin", keep_path=False)
+        self.copy("*gmock.pdb", dst="bin", keep_path=False)
+        self.copy("*gtest_main.pdb", dst="bin", keep_path=False)
+        self.copy("*gmock_main.pdb", dst="bin", keep_path=False)
+        self.copy("*gtestd.pdb", dst="bin", keep_path=False)
+        self.copy("*gmockd.pdb", dst="bin", keep_path=False)
+        self.copy("*gtest_maind.pdb", dst="bin", keep_path=False)
+        self.copy("*gmock_maind.pdb", dst="bin", keep_path=False)
+
+    def package_id(self):
+        self.info.options.with_unit_tests = "any"
+        self.info.options.ninja = "any"
+
+    def package_info(self):
+        self.cpp_info.libs = ["gmock_main"] if self.settings.build_type == "Release" else ["gmock_maind"]
+        if self.settings.os == "Linux":
+            if self.settings.build_type == "Release":
+                self.cpp_info.libs.extend(["gmock", "gtest"])
+            else:
+                self.cpp_info.libs.extend(["gmockd", "gtestd"])
+            self.cpp_info.libs.append("pthread")
+
