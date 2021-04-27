a627cfa9c316b3f787fffcf22c4ff20a7c1c5144
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-04-27 03:15:14 +0300

    Fix build without Ninja

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import os
 
 class GoogletestConan(ConanFile):
     name = "fmt"
-    version = "7.1.3+0"
+    version = "7.1.3+1"
     license = "https://raw.githubusercontent.com/fmtlib/fmt/master/LICENSE.rst"
     description = "{fmt} is an open-source formatting library for C++. It can be used as a safe and fast alternative to (s)printf and iostreams."
     url = "https://github.com/odant/conan-fmt"
@@ -31,7 +31,7 @@ class GoogletestConan(ConanFile):
     no_copy_source = True
     build_policy = "missing"
 
-    def build_requiments(self):
+    def build_requirements(self):
         if self.options.ninja:
             self.build_requires("ninja/[>=1.9.0]")
 
